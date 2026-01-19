8dbdb020bd19efe5460244826f225ba7e3779958
Aarush Yeruva <aarushyeruva@Aarushs-MacBook-Air-2.local>
2026-01-19 12:42:39 -0500

    What do you want to eat?

---
+++ meal-planner.py
@@ -0,0 +1,12 @@
+# Enter your code here# Enter your code here
+
+meal = input("breakfast, lunch or dinner: ")
+
+if meal == "breakfast":
+    print("How about some avacado toast? ")
+elif meal == "lunch":
+    print("which sandwich? ")
+elif meal == "dinner":
+    print("How about soup?")
+else:
+    print("That is not a valid response")
